c4db420e94716c7dd2cfd9f2a316f39a8d934fe0
twgd <twgd.huang@gmail.com>
2024-07-26 01:51:09 +0800

    feat: initialized Next.js project with basic setup

---
+++ .lintstagedrc.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,mjs,ts,tsx,json,css}': ['prettier --write'],
+}
